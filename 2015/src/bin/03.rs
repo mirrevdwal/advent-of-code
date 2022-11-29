9d4599e2c537d7758b9f0b0c9415abb5154339d1
Mirre van der Wal <mirrevanderwal@gmail.com>
2022-11-29 15:05:53 +0100

    Improve 2015 - 03

--- 2015/src/bin/03.rs
+++ 2015/src/bin/03.rs
@@ -8,43 +8,43 @@ fn main() {
     coords.insert(current);
 
     data.lines().for_each(|line| {
-	line.chars().for_each(|character| {
-	    if character == '^' {
-		current.1 += 1;
-	    } else if character == '>' {
-		current.0 += 1;
-	    } else if character == 'v' {
-		current.1 -= 1;
-	    } else {
-		current.0 -= 1;
-	    }
-	    coords.insert(current);
-	})
+        line.chars().for_each(|character| {
+            if character == '^' {
+                current.1 += 1;
+            } else if character == '>' {
+                current.0 += 1;
+            } else if character == 'v' {
+                current.1 -= 1;
+            } else {
+                current.0 -= 1;
+            }
+            coords.insert(current);
+        })
     });
 
     let num_houses = coords.len();
 
     println!("Part 1: {}", num_houses);
 
-    let mut current: Vec<(i32, i32)> = vec![(0,0), (0,0)];
+    let mut current: Vec<(i32, i32)> = vec![(0, 0), (0, 0)];
     let mut index = 0;
     let mut coords: HashSet<(i32, i32)> = HashSet::new();
 
     data.lines().for_each(|line| {
-	line.chars().for_each(|character| {
-	    let person = index % 2;
-	    if character == '^' {
-		current[person].1 += 1;
-	    } else if character == '>' {
-		current[person].0 += 1;
-	    } else if character == 'v' {
-		current[person].1 -= 1;
-	    } else {
-		current[person].0 -= 1;
-	    }
-	    coords.insert(current[person]);
-	    index += 1;
-	})
+        line.chars().for_each(|character| {
+            let person = index % 2;
+            if character == '^' {
+                current[person].1 += 1;
+            } else if character == '>' {
+                current[person].0 += 1;
+            } else if character == 'v' {
+                current[person].1 -= 1;
+            } else {
+                current[person].0 -= 1;
+            }
+            coords.insert(current[person]);
+            index += 1;
+        })
     });
 
     let num_houses = coords.len();
