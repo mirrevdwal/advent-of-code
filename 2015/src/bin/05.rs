7431c07e990ca3b9deeae2c580f30e5a4ae64c99
Mirre van der Wal <mirrevanderwal@gmail.com>
2022-11-29 15:11:27 +0100

    Improve 2015 - 05

--- 2015/src/bin/05.rs
+++ 2015/src/bin/05.rs
@@ -11,25 +11,31 @@ fn part_one() {
     let vowels = ['a', 'e', 'i', 'o', 'u'];
     let bad_combinations = ["ab", "cd", "pq", "xy"];
 
-    let nice = data.lines().filter(|line| {
-	for bad_combination in bad_combinations {
-	    if line.contains(bad_combination) {
-		return false;
-	    }
-	}
+    let nice = data
+        .lines()
+        .filter(|line| {
+            for bad_combination in bad_combinations {
+                if line.contains(bad_combination) {
+                    return false;
+                }
+            }
 
-	if !line.chars().zip(line.chars().skip(1)).any(|(x, y)| x == y) {
-	    return false;
-	}
+            if !line.chars().zip(line.chars().skip(1)).any(|(x, y)| x == y) {
+                return false;
+            }
 
-	if line.chars().filter(|character| vowels.contains(character)).count() < 3
-	{
-	    return false;
-	};
+            if line
+                .chars()
+                .filter(|character| vowels.contains(character))
+                .count()
+                < 3
+            {
+                return false;
+            };
 
-	true
-
-    }).count();
+            true
+        })
+        .count();
 
     println!("Part 1: {}", nice);
 }
@@ -37,25 +43,30 @@ fn part_one() {
 fn part_two() {
     let data = std::fs::read_to_string("data/05.txt").expect("Could not read datafile");
 
-    let nice = data.lines().filter(|line| {
-	let characters: Vec<char> = line.chars().collect();
+    let nice = data
+        .lines()
+        .filter(|line| {
+            let characters: Vec<char> = line.chars().collect();
 
-	if !characters.windows(3).any(|window| window[0] == window[2]) {
-	    return false;
-	}
+            if !characters.windows(3).any(|window| window[0] == window[2]) {
+                return false;
+            }
 
-	let pairs: HashSet<_> = characters.windows(2).collect();
-	let triplets = characters.windows(4).filter(|window| {
-	    window[0] == window[1] && window[1] == window[2] && window[2] != window[3]
-	}).count();
+            let pairs: HashSet<_> = characters.windows(2).collect();
+            let triplets = characters
+                .windows(4)
+                .filter(|window| {
+                    window[0] == window[1] && window[1] == window[2] && window[2] != window[3]
+                })
+                .count();
 
-	if pairs.len() + triplets == line.len() - 1 {
-	    return false;
-	}
+            if pairs.len() + triplets == line.len() - 1 {
+                return false;
+            }
 
-	true
-
-    }).count();
+            true
+        })
+        .count();
 
     println!("Part 2: {}", nice);
 }
