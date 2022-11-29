1d1e1e0c5e9b819a78a4af1c13fac36f0e6071ff
Mirre van der Wal <mirrevanderwal@gmail.com>
2022-11-29 15:07:00 +0100

    Improve 2015 - 04

--- 2015/src/bin/04.rs
+++ 2015/src/bin/04.rs
@@ -1,7 +1,7 @@
 fn main() {
     let data: String = std::fs::read_to_string("data/04.txt").expect("Could not read datafile");
 
-    let string: &str = data.lines().nth(0).expect("Could not find first line");
+    let string: &str = data.lines().next().expect("Could not find first line");
 
     part_one(string);
     part_two(string);
